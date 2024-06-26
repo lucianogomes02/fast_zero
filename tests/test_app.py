7abab7029c297469d9a09d76b11b66d2e9c8145a
Luciano Gomes <lucianogvda02@gmail.com>
2024-06-26 20:52:02 -0300

    test: implements create_user test

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,20 +1,13 @@
-from fastapi.testclient import TestClient
 from starlette import status
 
-from fast_zero.app import app
 
-
-def test_read_root_must_return_hello_world_and_status_code_200():
-    client = TestClient(app)
-
+def test_read_root_must_return_hello_world_and_status_code_200(client):
     response = client.get("/")
     assert response.status_code == status.HTTP_200_OK
     assert response.json() == {"message": "Hello, World!"}
 
 
-def test_create_user_must_return_user_and_status_code_201():
-    client = TestClient(app)
-
+def test_create_user_must_return_user_and_status_code_201(client):
     user_data = {
         "username": "John Doe",
         "email": "john.doe@test.com",
