dda796b21e99b6bc6789a65f96201ee6d8cb7c05
Luciano Gomes <lucianogvda02@gmail.com>
2024-06-26 20:42:16 -0300

    feat: implements create_user resource and schemas

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,6 +1,7 @@
 from fastapi.testclient import TestClient
-from fast_zero.app import app
 from starlette import status
+
+from fast_zero.app import app
 
 
 def test_read_root_must_return_hello_world_and_status_code_200():
@@ -8,4 +9,4 @@ def test_read_root_must_return_hello_world_and_status_code_200():
 
     response = client.get("/")
     assert response.status_code == status.HTTP_200_OK
-    assert response.json() == {"message": "Hello, World... again"}
+    assert response.json() == {"message": "Hello, World!"}
