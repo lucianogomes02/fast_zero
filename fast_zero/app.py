390fafc04111c02ccfa748ddb19a03b3ce8ca7e1
Luciano Gomes <lucianogvda02@gmail.com>
2024-06-26 20:40:13 -0300

    feat: implements create_user resource and schemas

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,11 +1,18 @@
 from fastapi import FastAPI
 from starlette import status
 
-from fast_zero.schemas import Message
+from fast_zero.schemas import Message, UserSchema, UserPublic
 
 app = FastAPI()
 
 
 @app.get("/", status_code=status.HTTP_200_OK, response_model=Message)
 def read_root():
-    return {"message": "Hello, World... again"}
+    return {"message": "Hello, World!"}
+
+
+@app.post(
+    "/users/", status_code=status.HTTP_201_CREATED, response_model=UserPublic
+)
+def create_user(user: UserSchema):
+    return user
