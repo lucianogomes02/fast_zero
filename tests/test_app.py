1a8f210cabbda6f1f391c44ebf0a8e832ab231fa
Luciano Gomes <lucianogvda02@gmail.com>
2024-06-26 20:48:44 -0300

    test: implements create_user test

--- tests/test_app.py
+++ tests/test_app.py
@@ -10,3 +10,21 @@ def test_read_root_must_return_hello_world_and_status_code_200():
     response = client.get("/")
     assert response.status_code == status.HTTP_200_OK
     assert response.json() == {"message": "Hello, World!"}
+
+
+def test_create_user_must_return_user_and_status_code_201():
+    client = TestClient(app)
+
+    user_data = {
+        "username": "John Doe",
+        "email": "john.doe@test.com",
+        "password": "secret",
+    }
+
+    response = client.post("/users/", json=user_data)
+
+    assert response.status_code == status.HTTP_201_CREATED
+    assert response.json() == {
+        "username": "John Doe",
+        "email": "john.doe@test.com",
+    }
