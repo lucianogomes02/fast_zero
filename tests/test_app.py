6f599a13e50eb662f1e9e3c4bc83e4bab4104041
Luciano Gomes <lucianogvda02@gmail.com>
2024-07-30 22:42:41 -0300

    test: implements list_users test

--- tests/test_app.py
+++ tests/test_app.py
@@ -21,3 +21,10 @@ def test_create_user_must_return_user_and_status_code_201(client):
         "username": "John Doe",
         "email": "john.doe@test.com",
     }
+
+
+def test_list_user_must_return_all_users_and_status_code_200(client):
+    response = client.get("/users/")
+
+    assert response.status_code == status.HTTP_200_OK
+    assert response.json() == {"users": []}
